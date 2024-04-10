f075230b76ea81f949adb7357eeeee7d6f003020
Jose Rubio <jadrubio@gmail.com>
2024-04-10 13:09:02 -0400

    doubly linked list set up

---
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,33 @@
+package DoublyLinkedList;
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node{
+        int value;
+        Node next;
+        Node prev;
+
+        Node (int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+}
+
---
+++ src/DoublyLinkedList/Main.java
@@ -0,0 +1,9 @@
+package DoublyLinkedList;
+
+public class Main {
+    public static void main(String[] args){
+        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+
+        myDLL.printList();
+    }
+}
