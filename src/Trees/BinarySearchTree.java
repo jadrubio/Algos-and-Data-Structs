4506141c140ade917c84cda33b511c7fa99dba71
Jose Rubio <jadrubio@gmail.com>
2024-04-10 16:29:25 -0400

    binary search tree

---
+++ src/Trees/BinarySearchTree.java
@@ -0,0 +1,64 @@
+package Trees;
+
+public class BinarySearchTree {
+     class Node {
+         int value;
+         Node left;
+         Node right;
+
+         Node(int value) {
+             this.value = value;
+         }
+     }
+
+     // Since we are setting root to null no constructor is needed
+     Node root;
+
+     public boolean insert(int value){
+         Node newNode = new Node(value);
+         if(root == null){
+             root = newNode;
+             return true;
+         }
+         Node temp = root;
+         while (true){
+             if (newNode.value == temp.value) return false;
+             if (newNode.value < temp.value) {
+                 if (temp.left == null){
+                     temp.left = newNode;
+                     return true;
+                 }
+                 temp = temp.left;
+             } else {
+                if (temp.right == null){
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+             }
+         }
+     }
+
+     public boolean contains(int value){
+        if (root == null) return false;
+         Node temp = root;
+         while (temp != null){
+             if (value == temp.value) return true;
+             if (value < temp.value) {
+                 temp = temp.left;
+             } else {
+                 temp = temp.right;
+             }
+         }
+         return false;
+     }
+
+    void printTree(Node node)
+    {
+        if(node == null) return;
+
+        System.out.println( node.value);
+        printTree(node.left);
+        printTree(node.right);
+    }
+}
---
+++ src/Trees/Main.java
@@ -0,0 +1,26 @@
+package Trees;
+
+public class Main {
+    public static void main(String[] args){
+        bst();
+    }
+
+    public static void bst() {
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        System.out.println("Root = " + myBST.root);
+
+        System.out.println("INSERT");
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(81);
+        myBST.printTree(myBST.root);
+
+        System.out.println("CONTAINS");
+        System.out.println("Does it contain 18? " + myBST.contains(18));
+        System.out.println("Does it contain 311? " + myBST.contains(311));
+    }
+}
